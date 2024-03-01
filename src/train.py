0ecce19e2341bec6ccb5e168d0df5f29595179e1
MarwanTRG <67915845+Azerty-ctrl-dot@users.noreply.github.com>
2024-03-01 19:07:50 +0100

    Update train.py

--- src/train.py
+++ src/train.py
@@ -49,23 +49,19 @@ class DQN(nn.Module):
     def __init__(self, n_observations, n_actions):
         super(DQN, self).__init__()
 
-        self.layer1 = nn.Linear(n_observations, 256)
-        self.layer2 = nn.Linear(256, 256)
-        self.layer3 = nn.Linear(256, 256)
-        self.layer4 = nn.Linear(256, 256)
-        self.layer5 = nn.Linear(256, 256)
-        self.layer6 = nn.Linear(256, 256)
-        self.layer7 = nn.Linear(256, n_actions)
+        self.layer1 = nn.Linear(n_observations, 512)
+        self.layer2 = nn.Linear(512, 512)
+        self.layer3 = nn.Linear(512, 512)
+        self.layer4 = nn.Linear(512, 512)
+        self.layer5 = nn.Linear(512, n_actions)
 
     def forward(self, x):
         x = F.relu(self.layer1(x))
         x = F.relu(self.layer2(x))
         x = F.relu(self.layer3(x))
         x = F.relu(self.layer4(x))
-        x = F.relu(self.layer5(x))
-        x = F.relu(self.layer6(x))
 
-        return self.layer7(x)
+        return self.layer5(x)
 
 
 # BATCH_SIZE is the number of transitions sampled from the replay buffer
